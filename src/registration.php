2ce70fdfb1416b99bf27f7d1b08e093a6b151c44
Alex Gusev <alex@flancer64.com>
2016-11-28 16:59:00 +0200

    init repo

---
+++ src/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Script to register M2-module
+ *
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    \Flancer32\Repo\Config::MODULE,
+    __DIR__
+);
