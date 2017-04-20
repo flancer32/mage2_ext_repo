7c2f82354c7ac6d4b82ed8301a7d3e42af5b2319
Alex Gusev <alex@flancer64.com>
2017-04-20 09:10:32 +0300

    Improve grid for logs

---
+++ src/Repo/Query/Expression.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Flancer32\Lib\Repo\Repo\Query;
+
+use Magento\Rule\Model\Condition\Sql\Expression as BaseExpression;
+
+/**
+ * Library wrapper for SQL expressions.
+ */
+class Expression extends BaseExpression
+{
+
+}
