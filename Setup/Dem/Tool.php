ccdc8b034e6398a7f9518f527b1b53b1cdd64beb
Alex Gusev <alex@flancer64.com>
2017-05-08 22:01:19 +0300

    OWN-80 - LoginAs: uninstall DB structures

--- Setup/Dem/Tool.php
+++ Setup/Dem/Tool.php
@@ -20,6 +20,8 @@ class Tool
     protected $parser;
     /** @var \Magento\Framework\App\ResourceConnection */
     protected $resource;
+    /** @var \Magento\Framework\DB\Adapter\AdapterInterface */
+    protected $conn;
 
     /**
      * Tool constructor.
@@ -30,7 +32,7 @@ class Tool
         \Flancer32\Lib\Repo\Setup\Dem\Parser $parser
     ) {
         $this->resource = $resource;
-        $this->_conn = $resource->getConnection();
+        $this->conn = $resource->getConnection();
         $this->logger = $logger;
         $this->parser = $parser;
     }
@@ -45,7 +47,7 @@ class Tool
         $tblName = $this->resource->getTableName($entityAlias);
         $this->logger->info("Create new table: $tblName.");
         /* init new object to create table in DB */
-        $tbl = $this->_conn->newTable($tblName);
+        $tbl = $this->conn->newTable($tblName);
         if (isset($demEntity[DemCfg::COMMENT])) {
             $tbl->setComment($demEntity[DemCfg::COMMENT]);
         }
@@ -71,13 +73,13 @@ class Tool
                 $ndxFields = $this->parser->entityGetIndexFields($ndx);
                 $ndxType = $this->parser->entityGetIndexType($ndx);
                 $ndxOpts = $this->parser->entityGetIndexOptions($ndx);
-                $ndxName = $this->_conn->getIndexName($entityAlias, $ndxFields, $ndxType);
+                $ndxName = $this->conn->getIndexName($entityAlias, $ndxFields, $ndxType);
                 $this->logger->info("Create new index: $ndxName.");
                 $tbl->addIndex($ndxName, $ndxFields, $ndxOpts);
             }
         }
         /* create new table */
-        $this->_conn->createTable($tbl);
+        $this->conn->createTable($tbl);
         /* parse relations */
         if ($relations) {
             foreach ($relations as $one) {
@@ -90,10 +92,10 @@ class Tool
                 /* there is no onUpdate in M2, $purge is used instead. Set default value 'false' for purge. */
                 //$onUpdate = $this->_parser->referenceGetAction($one[DemCfg::ACTION][DemCfg::UPDATE]);
                 $onUpdate = false;
-                $fkName = $this->_conn->getForeignKeyName($tblName, $ownColumn, $refTable, $refColumn);
+                $fkName = $this->conn->getForeignKeyName($tblName, $ownColumn, $refTable, $refColumn);
                 $this->logger->info("Create new relation '$fkName' from '$tblName:$ownColumn' to '$refTable:$refColumn'.");
                 try {
-                    $this->_conn->addForeignKey($fkName, $tblName, $ownColumn, $refTable, $refColumn, $onDelete,
+                    $this->conn->addForeignKey($fkName, $tblName, $ownColumn, $refTable, $refColumn, $onDelete,
                         $onUpdate);
                 } catch (\Exception $e) {
                     $msg = "Cannot create FK '$fkName'. Error: " . $e->getMessage();
