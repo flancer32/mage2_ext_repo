3a1940d1d28e52b87559b7e251b176271d674221
Alex Gusev <alex@flancer64.com>
2017-06-27 10:01:51 +0300

    SUPPLZ-133 - Create "previously ordered products" list

---
+++ Fw/Db/Select.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Flancer32\Lib\Repo\Fw\Db;
+
+/**
+ * Wrapper for Magento wrapper for \Zend_Db_Select. This wrapper is used inside this module instead of Magento wrapper.
+ */
+class Select
+    extends \Magento\Framework\DB\Select
+{
+
+}
--- Repo/Query/IBuilder.php
+++ Repo/Query/IBuilder.php
@@ -6,15 +6,25 @@
 namespace Flancer32\Lib\Repo\Repo\Query;
 
 /**
- * Interface for selection query builders. Queries can be based on other queries.
+ * Interface for selection query builders. Queries can be based on other selection queries.
  */
 interface  IBuilder
 {
+    /**
+     * Build selection query (optionally) based on other query.
+     *
+     * @param \Flancer32\Lib\Repo\Fw\Db\Select|null $source
+     * @return \Flancer32\Lib\Repo\Fw\Db\Select
+     */
+    public function build(\Flancer32\Lib\Repo\Fw\Db\Select $source = null);
+
     /**
      * Get SELECT COUNT query.
      *
      * @param \Flancer32\Lib\Repo\Repo\Query\IBuilder $qbuild
      * @return \Magento\Framework\DB\Select
+     *
+     * @deprecated use 'build' method instead.
      */
     public function getCountQuery(\Flancer32\Lib\Repo\Repo\Query\IBuilder $qbuild = null);
 
@@ -23,6 +33,8 @@ interface  IBuilder
      *
      * @param \Flancer32\Lib\Repo\Repo\Query\IBuilder $qbuild
      * @return \Magento\Framework\DB\Select
+     *
+     * @deprecated use 'build' method instead.
      */
     public function getSelectQuery(\Flancer32\Lib\Repo\Repo\Query\IBuilder $qbuild = null);
 
